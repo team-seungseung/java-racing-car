8ce199b46e3b4c52c8a3475d814db7e427c3bea8
ohsing <ohsing@ohsingui-MacBookAir.local>
2020-03-22 03:16:39 +0900

    JAVA-5 indet 2>1

--- src/main/java/Racing.java
+++ src/main/java/Racing.java
@@ -3,14 +3,35 @@ import java.util.Random;
 
 public class Racing {
 
-    public static void run(int[] carPositions, int trying) {
-        Random random = new Random();
+    public static void runCar(int[] carPositions, int trying) {
+        for (int carnum = 0; carnum < carPositions.length; carnum++) {
+            tryingTurn(carPositions, trying, carnum);
+        }
+    }
 
-        for(int i=0; i<carPositions.length; i++) {
-            for (int chance = 0; chance < trying; chance++){
-                if(random.nextInt(10) >= 4)
-                    carPositions[i]++;
-            }
+    public static void tryingTurn(int[] carPositions, int trying, int carnum){
+        for (int chance = 0; chance < trying; chance++) {
+            comparingValue(carPositions, carnum);
+        }
+    }
+
+    public static void comparingValue(int[] carPositions, int carnum){
+        Random random = new Random();
+        if (random.nextInt(10) >= 4)
+            carPositions[carnum]++;
+    }
+
+    public static void printCar(int[] carPositions) {
+        System.out.println("실행 결과");
+        for (int car = 0; car < carPositions.length; car++) {
+            printDash(carPositions, car);
+            System.out.println();
+        }
+    }
+
+    public static void printDash(int[] carPositions, int car){
+        for (int position = 0; position < carPositions[car]; position++) {
+            System.out.print('-');
         }
     }
 
@@ -27,14 +48,8 @@ public class Racing {
         System.out.println("시도할 회수는 몇 회 인가요?");
         trying = sc.nextInt();
 
-        run(carPositions,trying);
+        runCar(carPositions, trying);
+        printCar(carPositions);
 
-        System.out.println("실행 결과");
-        for(int i=0; i<carPositions.length; i++) {
-            for(int j=0; j < carPositions[i]; j++){
-                System.out.print('-');
-            }
-            System.out.println();
-        }
     }
 }
